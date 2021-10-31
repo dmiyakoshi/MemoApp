a9ad35241a56f4678f2304e9816d0387cf73bf06
dmiyakoshi <d1m354dai@gmail.com>
2021-10-31 11:37:32 +0900

    メモアプリでコミット

--- MemoApp/ViewController.swift
+++ MemoApp/ViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextViewDelegate {
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textView.delegate = self //selfはクラス全体を指している
+        
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate
+        textView.text = sceneDelegate.lastText
     }
 
-
+    func textViewDidChange(_ textView: UITextView) {
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate
+        sceneDelegate.lastText = textView.text
+    }
 }
 
